285ef72f8eaae0b6bed1f09b423e54a7eb1c246c
pathhen901230 <enrique.delgadilloest@gmail.com>
2024-12-12 00:23:10 -0600

    Update Start.py

--- Start.py
+++ Start.py
@@ -19,7 +19,12 @@ def prime(x):
     return(True)    
 
 prime(7)
-
+#Paquetes necesarios para realizar operaciones
+import matplotlib.pyplot as plt
+import scipy.stats as sp
+import numpy as np
+import pandas as pd
+import os
 #Funciones para hacer calculos
 
 #Funcion de asociacion
@@ -37,6 +42,42 @@ def pre_esti_AS(cr):
     C975= np.exp(np.log(OR)+(1.96*errstd))
     out= pd.DataFrame([{'Iex':Iex,'Ienox':Ienox,'OR':OR,'C025':C025,'C975':C975}])
     return(out)
+#Funcion de Wald
+    #Devuelve intervalo de confianza con método de Wald
+def pre_esti_propwald(p,n):
+    if p>1:
+        print('p tiene que ser igual o menor a 1')
+    else:
+        med= p
+        errstd= np.sqrt( (p*(1-p))/n )
+        c025= med - 1.96*errstd
+        c975= med + 1.96*errstd
+        dic= pd.DataFrame([{'Proporcion':med,'C025':c025,'C975':c975}])
+        return(dic)
+#Funcion para sacar la prevalencia con intervalos de confianza de todo el data.frame
+    #Devuelve eventos, prevalencia e intervalos de confianza de un df. Requiere funcion pre_esti_propwald.
+def pre_des_mdicot(df):
+    dic = pd.DataFrame([{'Columna':'NA','Eventos':'NA', 
+                       "Proporcion":'NA',
+                       'IC025':'NA','IC975':'NA'}])
+    for i in range(0,len(df.columns)):
+        try:                  
+            ev= sum(df.iloc[:,i])
+            prop= round(df.iloc[:,i].mean(),4)
+            ic= pre_esti_propwald(prop,len(df))
+            ic025= ic.iloc[0,1]
+            ic975= ic.iloc[0,2]
+            rv= pd.DataFrame([{'Columna':df.columns[i],'Eventos':ev, 
+                               "Proporcion":prop,
+                               'IC025':ic025,'IC975':ic975}])
+            dic = dic._append(rv)
+        except:
+            rv= pd.DataFrame([{'Columna':df.columns[i],'Eventos':'NA', 
+                               "Proporcion":'NA',
+                               'IC025':'NA','IC975':'NA'}])
+            dic= dic._append(rv) 
+    return(dic)        
+
 #Función de prueba diagnóstica
     #Devuelve la sensibilidad, especificidad, valor predictivo positivo, valor predictivo negativo, precision y J
 def pre_pdiag(cr):
@@ -52,24 +93,24 @@ def pre_pdiag(cr):
     return(Out)
 #Funcion media y desviación estandar
     #Devuelve la media y la desviación estándar del data.frame
-def pre_medsd(df):
+def pre_des_medsd(df):
     med= round(df.mean(),4)
     sd= round(df.std(),4)
     d= {'Media':med,'Desviacion':sd}
     dictio = pd.DataFrame([d])
     return(dictio)
 #Funcion para devolver media y desviación estándar de todo el data.frame
-def pre_tab_medf(df):
+def pre_des_medf(df):
     di= pd.DataFrame([{'Col':'NA','Media':'NA','Sd':'NA'}])
     for i in range(0,len(df.columns)):
         try:
-            out= pre_medsd(df.iloc[:,i])
+            out= pre_des_medsd(df.iloc[:,i])
             apt= pd.DataFrame([{'Col':df.columns[i],
                            'Media':out.iloc[0,0],
                            'Sd':out.iloc[0,1]}])
             di= di._append(apt)
             print(df.columns[i])
-            print(pre_medsd(df.iloc[:,i]))
+            print(pre_des_medsd(df.iloc[:,i]))
         except:
             print(df.columns[i])
             print('NA')
