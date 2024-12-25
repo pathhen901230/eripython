a9284a0dfd1a55efbcb7c2f23fdee8b1d4706ef0
pathhen901230 <enrique.delgadilloest@gmail.com>
2024-12-25 16:29:53 -0600

    Update pre_class.py

--- pre_class.py
+++ pre_class.py
@@ -21,7 +21,30 @@ class estimate_OR:
         ic975= np.log(OR) + (1.96*std)
         self.Iex= (a+b)/tot
         self.Ienox= (c+d)/tot
-        self.media= OR
+        self.OR= OR
         self.ic025= ic025
         self.ic975= ic975
+
+#Clase estimate RR. Requiere aportar una tabla de contingencias
+class estimate_RR:
+    def __init__(self,tab):
+        a= tab.iloc[1,1]
+        b= tab.iloc[1,0]
+        c= tab.iloc[0,1]
+        d=tab.iloc[0,0]
+        Iex= a/(a+b)
+        Ienox= c/(c+d)
+        tot= a+b+c+d
+        RR= Iex/Ienox
+        FE= (1-Iex)/a
+        SE= (1-Ienox)/c
+        std= np.sqrt(FE+SE)
+        ic025= np.exp(np.log(RR) - (1.96*std))
+        ic975= np.exp(np.log(RR) + (1.96*std))
+        self.Iex= a/(a+b)
+        self.Ienox= c/(c+d)
+        self.RR= RR
+        self.ic025= ic025
+        self.ic975= ic975
+
     
