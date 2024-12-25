ff478ff6cd969d9f4d74b8240c96bc2a9cb448be
pathhen901230 <enrique.delgadilloest@gmail.com>
2024-12-25 17:38:06 -0600

    Update pre_class.py

--- pre_class.py
+++ pre_class.py
@@ -8,30 +8,52 @@ import pandas as pd
 import os
 #Dependencias
 #Generar objeto de Odds/Ratio
+#Clase estimate_OR 
 class estimate_OR:
     def __init__(self,tab):
         a= tab.iloc[1,1]
         b= tab.iloc[1,0]
         c= tab.iloc[0,1]
         d=tab.iloc[0,0]
+        #Valoracion de supuesto Fisher
+        fish_bool= a>5 and b>5 and c>5 and d>5
+        if fish_bool== True:
+            #Chi_cuadrada
+            self.p=stats.chi2_contingency(tab)[1]
+            self.method="Chi Cuadrada"
+        else:
+            #Prueba exacta de Fisher
+            self.p=stats.fisher_exact(tab)[1]
+            self.method="Exacta de Fisher"
         tot= a+b+c+d
         OR= (a*d)/(b*c)
         std= np.sqrt((1/a)+(1/+b)+(1/c)+(1/d))
-        ic025= np.log(OR) - (1.96*std)
-        ic975= np.log(OR) + (1.96*std)
-        self.Iex= (a+b)/tot
-        self.Ienox= (c+d)/tot
+        ic025= np.exp(np.log(OR) - (1.96*std))
+        ic975= np.exp(np.log(OR) + (1.96*std))
+        self.Pre= (a+b)/tot
         self.OR= OR
         self.ic025= ic025
         self.ic975= ic975
-
-#Clase estimate RR. Requiere aportar una tabla de contingencias
+#Prueba estimate_OR    
+es=estimate_OR(pd.crosstab(df['DIABETES 0-NO 1-SI '],df['HAS 0-NO 1-SI ']))
+#Generar objeto de Riesgo relativo
 class estimate_RR:
     def __init__(self,tab):
+
         a= tab.iloc[1,1]
         b= tab.iloc[1,0]
         c= tab.iloc[0,1]
         d=tab.iloc[0,0]
+        #Valoracion de supuesto Fisher
+        fish_bool= a>5 and b>5 and c>5 and d>5
+        if fish_bool== True:
+            #Chi_cuadrada
+            self.p=stats.chi2_contingency(tab)[1]
+            self.method="Chi Cuadrada"
+        else:
+            #Prueba exacta de Fisher
+            self.p=stats.fisher_exact(tab)[1]
+            self.method="Exacta de Fisher"
         Iex= a/(a+b)
         Ienox= c/(c+d)
         tot= a+b+c+d
@@ -46,5 +68,9 @@ class estimate_RR:
         self.RR= RR
         self.ic025= ic025
         self.ic975= ic975
+    def printf(self):
+        print(('Iex:'+str(self.Iex)+', Ienox:'+str(self.Ienox)
+              +' ,RR:'+str(self.RR)+
+              ' ,IC025:'+str(self.ic025)+' ,IC975:'+str(self.ic975)))
 
     
